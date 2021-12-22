413fd81bce7faeff080c3f633813128090d84ce1
Abhishek Jha <Abhishek@Jha.com>
2021-12-22 20:25:34 +0530

    Added single page templates

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import render
 def starting_page(request):
     return render(request,"blog/index.html")
 
-def posts():
-    pass
+def posts(request):
+    return render(request,"blog/all-posts.html")
 
-def post_detail():
-    pass
+def post_detail(request,slug):
+    return render(request,"blog/post-detail.html")
 
